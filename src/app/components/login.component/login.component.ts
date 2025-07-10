4704eee5cc6f001249391689edfcc7427db58f2b
Linos Darikai <linostdarikai@gmail.com>
2025-07-10 15:44:17 +0200

    Added the page schema for the loginPage

---
+++ src/app/components/login.component/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, input } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+  
+  
+}
