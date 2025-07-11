1b68ddf388e80a74a08a09e273215276471cf483
Linos Darikai <linostdarikai@gmail.com>
2025-07-11 19:38:32 +0200

    Added an primeng components and tailwind css to the project config

--- src/app/components/login.component/login.component.ts
+++ src/app/components/login.component/login.component.ts
@@ -1,14 +1,21 @@
 import { Component, input } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, InputTextModule, MessageModule],
   standalone: true,
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+isInvalid(arg0: string) {
+  return true;
+}
+onSubmit() {
+throw new Error('Method not implemented.');
+}
   loginForm = new FormGroup({
     email: new FormControl(''),
     password: new FormControl(''),
