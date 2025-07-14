3552f48e40c7eed92dfa5ca91afe99383d23b0e8
Phil Kazembe <kaz@Phils-MacBook-Pro.local>
2025-07-14 11:27:11 +0200

    side nav bar UI complete

---
+++ src/app/pages/employee-dashboard/employee-dashboard.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SideNavBar } from '../../components/side-nav-bar/side-nav-bar';
+
+@Component({
+  selector: 'app-employee-dashboard',
+  imports: [],
+  templateUrl: './employee-dashboard.html',
+  styleUrl: './employee-dashboard.css'
+})
+export class EmployeeDashboard {
+
+}
