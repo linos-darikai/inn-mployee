2616892706e2b67eb628b67c14a4c1e86be816d8
Linos Darikai <linostdarikai@gmail.com>
2025-07-14 12:17:56 +0200

    Created the page and componets for the ResetPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login.page/login.page';
 import { EmployeeDashboard } from './pages/employee-dashboard/employee-dashboard';
 import { DashboardLayout } from './layouts/dashboard-layout/dashboard-layout';
+import { ResetPasswordPage } from './pages/reset-password.page/reset-password.page';
 
 export const routes: Routes = [
-    // TEMPORARILY CHANGED FROM LOGINPAGE TO EMPLOYEE DASHBOARD
-    { path: '', component: DashboardLayout }, // To be changed back to LoginPage
+    // TEMPORARILY CHANGED FROM Dashboard to ResetPage
+    { path: '', component: ResetPasswordPage }, // To be changed back to LoginPage
 ];
