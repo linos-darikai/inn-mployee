27605c207d9ba64f23804c8158f81622926abf23
Phil Kazembe <kaz@Phils-MacBook-Pro.local>
2025-07-14 12:01:53 +0200

    sorted out sidenav styling to allow dashboard content to display

--- src/app/pages/employee-dashboard/employee-dashboard.ts
+++ src/app/pages/employee-dashboard/employee-dashboard.ts
@@ -3,10 +3,16 @@ import { SideNavBar } from '../../components/side-nav-bar/side-nav-bar';
 
 @Component({
   selector: 'app-employee-dashboard',
+  standalone: true,
   imports: [],
   templateUrl: './employee-dashboard.html',
   styleUrl: './employee-dashboard.css'
 })
 export class EmployeeDashboard {
+
+  // temporary user_data
+  user = {
+    name: 'Phil'
+  }
 
 }
