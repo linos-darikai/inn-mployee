02999ffb29e490669af06dd43f45cb0556ebb97b
Phil Kazembe <kaz@Phils-MacBook-Pro.local>
2025-07-15 09:52:52 +0200

    todo-list done

--- src/app/pages/employee-dashboard/employee-dashboard.ts
+++ src/app/pages/employee-dashboard/employee-dashboard.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { Countdown } from '../../components/countdown/countdown';
 import { employeeTest } from '../../tools/objects';
 import { EmployeeProgressChart } from '../../components/employee-progress-chart/employee-progress-chart';
-
+import { EmployeeTodoList } from '../../components/employee-todo-list/employee-todo-list';
 
 @Component({
   selector: 'app-employee-dashboard',
   standalone: true,
-  imports: [Countdown, EmployeeProgressChart],
+  imports: [Countdown, EmployeeProgressChart, EmployeeTodoList],
   templateUrl: './employee-dashboard.html',
   styleUrl: './employee-dashboard.css'
 })
