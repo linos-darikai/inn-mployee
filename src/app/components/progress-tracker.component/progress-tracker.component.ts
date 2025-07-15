ad0052da45780d46c8b37ad1333a2251bef4db3b
Linos Darikai <linostdarikai@gmail.com>
2025-07-15 10:11:41 +0200

    Created the form components and template for the page

---
+++ src/app/components/progress-tracker.component/progress-tracker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-tracker.component',
+  imports: [],
+  templateUrl: './progress-tracker.component.html',
+  styleUrl: './progress-tracker.component.css'
+})
+export class ProgressTrackerComponent {
+
+}
---
+++ src/app/layouts/form-template-layout/form-template-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-template-layout',
+  imports: [],
+  templateUrl: './form-template-layout.html',
+  styleUrl: './form-template-layout.css'
+})
+export class FormTemplateLayout {
+
+}
