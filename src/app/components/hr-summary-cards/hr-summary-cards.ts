5f879f84d0dfd3a6dcb89e87b5d45b5deb53f6ca
Phil Kazembe <kaz@Phils-MacBook-Pro.local>
2025-07-15 12:44:39 +0200

    summary card UI done

--- src/app/components/hr-summary-cards/hr-summary-cards.ts
+++ src/app/components/hr-summary-cards/hr-summary-cards.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-hr-summary-cards',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './hr-summary-cards.html',
   styleUrl: './hr-summary-cards.css'
 })
