c3585e89bc37789338880879ba19c53ae1cfee98
Linos Darikai <linostdarikai@gmail.com>
2025-07-16 08:58:23 +0200

    Last commit forgetten progress

--- src/app/components/form-navbar.component/form-navbar.component.ts
+++ src/app/components/form-navbar.component/form-navbar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-form-navbar',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './form-navbar.component.html',
   styleUrl: './form-navbar.component.css'
 })
