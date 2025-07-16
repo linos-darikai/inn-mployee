53c4c4007a6b08e887313ed6f437479e7e044cae
Phil Kazembe <kaz@Phils-MacBook-Pro.local>
2025-07-16 11:29:27 +0200

    edit assignment portal UI done

--- src/app/components/hr-edit-form/hr-edit-form.ts
+++ src/app/components/hr-edit-form/hr-edit-form.ts
@@ -1,11 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { taskLists } from '../../tools/task-lists';
+import { MatSortModule } from '@angular/material/sort';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { PrimeIcons } from 'primeng/api';
 @Component({
   selector: 'app-hr-edit-form',
-  imports: [],
+  imports: [MatTableModule, CommonModule, MatSortModule, MatButtonModule, MatIconModule],
   templateUrl: './hr-edit-form.html',
   styleUrl: './hr-edit-form.css'
 })
 export class HrEditForm {
+  tasks: any;
+  displayedColumns = [ 'task'];
+
+  constructor() {
+    const tLists = [];
+
+    for(let i = 0; i < taskLists.length; i++){
+      for(let j = 0; j < taskLists[i].length; j++){
+        tLists.push(taskLists[i][j]);
+      }
+    }
+
+    this.tasks = new MatTableDataSource(tLists);
+  }
 
 }
