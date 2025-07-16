abab510f27bd728301068977052621db2dec00e5
Phil Kazembe <kaz@Phils-MacBook-Pro.local>
2025-07-16 08:57:15 +0200

    chart styling done

---
+++ src/app/components/hr-edit-form/hr-edit-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hr-edit-form',
+  imports: [],
+  templateUrl: './hr-edit-form.html',
+  styleUrl: './hr-edit-form.css'
+})
+export class HrEditForm {
+
+}
---
+++ src/app/components/hr-summary-table/hr-summary-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hr-summary-table',
+  imports: [],
+  templateUrl: './hr-summary-table.html',
+  styleUrl: './hr-summary-table.css'
+})
+export class HrSummaryTable {
+
+}
